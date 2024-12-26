ca1348e43ae4d3312bd75c903e0622608bce4fd8
CorePepper <becorep884@gmail.com>
2024-12-27 02:51:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // ← BrowserRouter を HashRouter に変更
 import Index from "./pages/Index";
 import Reviews from "./pages/Reviews";
 
@@ -13,12 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter> {/* ← BrowserRouter を HashRouter に変更 */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/reviews" element={<Reviews />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter> {/* ← BrowserRouter を HashRouter に変更 */}
     </TooltipProvider>
   </QueryClientProvider>
 );
